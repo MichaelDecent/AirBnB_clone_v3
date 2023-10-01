22876f076911096488741a2ddbfc49f974b5ee89
MichaelDecent <michaeldecent2@gmail.com>
2023-10-01 21:40:08 +0000

    added new endpoints for state object

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -2,20 +2,20 @@
 """
 This Module contains a view for state objects
 """
-from flask import jsonify, abort, request
+from flask import jsonify, abort, request, make_response
 from api.v1.views import app_views
 from models.state import State
 from models import storage
 
 
-@app_views.route('/states', methods=['GET'])
+@app_views.route('/states', methods=['GET'], strict_slashes=False)
 def get_states():
     """Retrieves the list of all State objects """
     states_objs = [obj.to_dict() for obj in storage.all(State).values()]
     return jsonify(states_objs)
 
 
-@app_views.route('/states/<state_id>', methods=['GET'])
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def get_state(state_id):
     """This retrieves the a state based of its ID """
     state = storage.get(State, state_id)
@@ -24,7 +24,7 @@ def get_state(state_id):
     return jsonify(state.to_dict())
 
 
-@app_views.route('/states/<state_id>', methods=['DELETE'])
+@app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
 def delete_state(state_id):
     """Deletes a state object based on a given ID"""
     state = storage.get(State, state_id)
@@ -32,10 +32,10 @@ def delete_state(state_id):
         abort(404)
     storage.delete(state)
     storage.save()
-    return jsonify({}), 200
+    return make_response(jsonify({}), 200)
 
 
-@app_views.route('/states', methods=['POST'])
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
 def post_states():
     """Creates a new state """
     request_json = request.get_json()
@@ -48,10 +48,10 @@ def post_states():
 
     state = State(**request_json)
     state.save()
-    return jsonify(state.to_dict()), 201
+    return make_response(jsonify(state.to_dict()), 201)
 
 
-@app_views.route('/states/<state_id>', methods=['PUT'])
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
 def put_state(state_id):
     """ this update a state object based on its state ID"""
     state = storage.get(State, state_id)
@@ -67,4 +67,4 @@ def put_state(state_id):
         if key not in ignore_keys:
             setattr(state, key, value)
     storage.save()
-    return jsonify(state.to_dict()), 200
+    return make_response(jsonify(state.to_dict()), 200)
