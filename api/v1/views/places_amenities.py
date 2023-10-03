0060c082eb2b760602543dedd146c3e9eaec4595
MichaelDecent <michaeldecent2@gmail.com>
2023-10-03 12:55:36 +0000

    added an endpoint for place_amenities

---
+++ api/v1/views/places_amenities.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+"""
+This Module contains a view that Place objects and
+Amenity objects that handles all default RESTFul API actions
+"""
+from flask import abort, make_response, jsonify
+from api.v1.views import app_views
+from models import storage, storage_t
+from models.amenity import Amenity
+from models.place import Place
+
+
+@app_views.route('places/<palce_id>/amenities', methods=['GET'], strict_slashes=False)
+def get_place_amenities(place_id):
+    """Retrieves all amenities of a particular place based on its ID"""
+    place = storage.get(Place, place_id)
+    if place is None:
+        abort(404)
+    if storage_t == "db":
+        amenities_list = [obj.to_dict() for obj in place.amenities]
+    else:
+        amenities_list = [storage.get(Amenity, amenity_id).to_dict() for amenity_id in place.amenity_ids]
+    return jsonify(amenities_list)
+
+
+@app_views.route('places/<place_id>/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+def delete_place_amenity(place_id, amenity_id):
+    """Deletes a Amenity object to a Place """
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+    if place is None or amenity is None:
+        abort(404)
+    if storage_t == "db":
+        if amenity not in place.amenities:
+            abort(404)
+        place.amenities.remove(amenity)
+       
+    else:
+        if amenity_id not in place.amenity_ids:
+            abort(404)
+        place.amenity_ids.remove(amenity_id)
+    
+    storage.save()
+    return make_response(jsonify({}), 200)
+
+
+@app_views.route('places/<place_id>/amenities/<amenity_id>', methods=['POST'], strict_slashes=False)
+def post_place_amenity(place_id, amenity_id):
+    """Link a Amenity object to a Place"""
+    place = storage.get(Place, place_id)
+    amenity = storage.get(Amenity, amenity_id)
+    if place is None or amenity is None:
+        abort(404)
+    if storage_t == "db":
+        if amenity in place.amenities:
+            return make_response(jsonify(amenity.to_dict()), 200)
+        place.amenities.append(amenity)
+    else:
+        if amenity_id in place.amenity_ids:
+            return make_response(jsonify(amenity.to_dict()), 200)
+        place.amenity_ids.append(amenity_id)
+    storage.save()
+    return make_response(jsonify(amenity.to_dict()), 201)
