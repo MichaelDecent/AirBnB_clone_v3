3834778e8d532e75ce93fd1e8af4ba44da9bede5
MichaelDecent <michaeldecent2@gmail.com>
2023-10-03 13:54:13 +0000

    fixed error

--- api/v1/views/places_amenities.py
+++ api/v1/views/places_amenities.py
@@ -10,7 +10,8 @@ from models.amenity import Amenity
 from models.place import Place
 
 
-@app_views.route('places/<place_id>/amenities', methods=['GET'], strict_slashes=False)
+@app_views.route('places/<place_id>/amenities',
+                 methods=['GET'], strict_slashes=False)
 def get_place_amenities(place_id):
     """Retrieves all amenities of a particular place based on its ID"""
     place = storage.get(Place, place_id)
@@ -19,11 +20,13 @@ def get_place_amenities(place_id):
     if storage_t == "db":
         amenities_list = [obj.to_dict() for obj in place.amenities]
     else:
-        amenities_list = [storage.get(Amenity, amenity_id).to_dict() for amenity_id in place.amenity_ids]
+        amenities_list = [storage.get(Amenity, amenity_id).to_dict()
+                          for amenity_id in place.amenity_ids]
     return jsonify(amenities_list)
 
 
-@app_views.route('places/<place_id>/amenities/<amenity_id>', methods=['DELETE'], strict_slashes=False)
+@app_views.route('places/<place_id>/amenities/<amenity_id>',
+                 methods=['DELETE'], strict_slashes=False)
 def delete_place_amenity(place_id, amenity_id):
     """Deletes a Amenity object to a Place """
     place = storage.get(Place, place_id)
@@ -34,17 +37,18 @@ def delete_place_amenity(place_id, amenity_id):
         if amenity not in place.amenities:
             abort(404)
         place.amenities.remove(amenity)
-       
+
     else:
         if amenity_id not in place.amenity_ids:
             abort(404)
         place.amenity_ids.remove(amenity_id)
-    
+
     storage.save()
     return make_response(jsonify({}), 200)
 
 
-@app_views.route('places/<place_id>/amenities/<amenity_id>', methods=['POST'], strict_slashes=False)
+@app_views.route('places/<place_id>/amenities/<amenity_id>',
+                 methods=['POST'], strict_slashes=False)
 def post_place_amenity(place_id, amenity_id):
     """Link a Amenity object to a Place"""
     place = storage.get(Place, place_id)
